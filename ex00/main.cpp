0f74bc90729e079d7284db98b63ed4c146956b09
zinb9419 <zinb9419@gmail.com>
2023-03-09 23:20:57 +0100

    cpp08

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 15:09:39 by ziloughm          #+#    #+#             */
-/*   Updated: 2023/03/09 22:33:15 by ziloughm         ###   ########.fr       */
+/*   Updated: 2023/03/09 23:20:43 by ziloughm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int main(void)
     std::vector<int> vec(nb, nb + (sizeof(nb) / sizeof(int)));
     std::deque<int> deq(nb, nb + (sizeof(nb) / sizeof(int)));
     std::list<int> lst(nb, nb + (sizeof(nb) / sizeof(int)));
-    std::stack<int> e;
 
     test(vec, 11, 1);
     test(deq, 2, 2);
